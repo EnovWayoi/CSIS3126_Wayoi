f3911fc6c7b8a79771dddf0bc0ec0d5c3d342864
Enov Wayoi <enovdilawayoi@gmail.com>
2026-02-24 04:15:45 -0500

    Refactor: Move tests to dedicated tests directory

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 import unittest
 from app import app, get_db_connection
 from werkzeug.security import generate_password_hash
-
 class WayoiTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
