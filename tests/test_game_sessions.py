692f025b17c0f12cda2989b91a1544ac78d70f13
Enov Wayoi <enovdilawayoi@gmail.com>
2026-04-13 01:37:01 -0400

    test(session): append static typing overrides for dynamic src resolution

--- tests/test_game_sessions.py
+++ tests/test_game_sessions.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import unittest
-from app import app, get_db_connection
+from app import app, get_db_connection  # type: ignore
 from werkzeug.security import generate_password_hash
 
 class GameSessionsTestCase(unittest.TestCase):
